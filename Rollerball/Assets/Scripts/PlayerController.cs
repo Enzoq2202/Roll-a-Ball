00cb1e376f73360309519eb2d242f3c5787c3da3
Enzo Quental <enzoq@al.insper.edu.br>
2024-03-14 22:17:24 -0300

    tutorial done

--- Rollerball/Assets/Scripts/PlayerController.cs
+++ Rollerball/Assets/Scripts/PlayerController.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody rb;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private float movementX;
     private float movementY;
 
     public float speed = 0;
 
+    private int count;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
 
@@ -28,10 +36,33 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
 
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
 
 
     }
